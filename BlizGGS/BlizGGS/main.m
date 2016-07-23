d5fc8282786259b87cacdff09de3d930e99d1526
KongBo <KongBo@me.com>
2016-07-23 15:01:07 +0800

    Squashed 'GGS/' content from commit debc28e
    git-subtree-dir: GGS
    git-subtree-split: debc28e8dd3362380f278a31434b56ab3d55f7ad

---
+++ BlizGGS/BlizGGS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlizGGS
+//
+//  Created by 孔博 on 16/7/23.
+//  Copyright © 2016年 KongBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GGSAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GGSAppDelegate class]));
+    }
+}
---
+++ BlizGGS/GGSBundle/GGSBundle.m
@@ -0,0 +1,13 @@
+//
+//  GGSBundle.m
+//  GGSBundle
+//
+//  Created by 孔博 on 16/7/23.
+//  Copyright © 2016年 KongBo. All rights reserved.
+//
+
+#import "GGSBundle.h"
+
+@implementation GGSBundle
+
+@end
