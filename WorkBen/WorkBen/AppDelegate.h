db9964f6df6b8477e88620fbba5652c9e7d091f6
KongBo <KongBo@me.com>
2016-07-23 10:07:24 +0800

    new program

---
+++ WorkBen/WorkBen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorkBen
+//
+//  Created by 孔博 on 16/7/23.
+//  Copyright © 2016年 KongBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorkBen/WorkBen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkBen
+//
+//  Created by 孔博 on 16/7/23.
+//  Copyright © 2016年 KongBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
